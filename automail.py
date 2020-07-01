59d0b301adb7c0476c21a06ebab4163a2d575b51
Omar Dakkak <43930902+OmarDakkak@users.noreply.github.com>
2020-07-01 17:59:00 +0100

    Update automail.py

--- automail.py
+++ automail.py
@@ -1,9 +1,16 @@
 import yagmail
+import glob
 import os
 print("sending email.....")
-receiver = "abdelfettah.hajjioui@gmail.com"  # receiver email address
+receiver = "omardakkak98@gmail.com"  # receiver email address
 body = "Attendence File"  # email body
-filename = "Attendance"+os.sep+"Attendance_2020-03-12_16-18-57.csv"  # attach the file
+
+list_of_files = glob.glob('Attendance/*') # * means all if need specific format then *.csv
+latest_file = max(list_of_files, key=os.path.getctime)
+
+
+latest_file = "/".join(latest_file.split("\\")) # attach the file
+
 
 # mail information
 yag = yagmail.SMTP("ehang01.bendahan@gmail.com", "dadi2019")
@@ -13,9 +20,7 @@ yag.send(
     to=receiver,
     subject="Attendance Report",  # email subject
     contents=body,  # email body
-    attachments=filename,  # file attached
+    attachments=latest_file,  # file attached
 )
 
 print("mail has been sent successfully")
-
-
